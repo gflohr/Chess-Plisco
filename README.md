f988930fbaf8043a7f86ca5ad574aae9790c449f
Guido Flohr <guido.flohr@cantanea.com>
2021-08-01 17:55:19 +0300

    initial project structure

---
+++ README.md
@@ -0,0 +1,12 @@
+# Chess-Position
+
+Chess::Position is a representation of a chess position in Perl.
+
+It only works with Perl versions that are compiled with support for 64
+bit integers!
+
+It is currently work in progress.
+
+## Copryight
+
+Copyright (C) 2021, Guido Flohr, guido.flohr@cantanea.com, all rights reserved.
---
+++ lib/Chess/Position.pm
@@ -0,0 +1,27 @@
+#! /bin/false
+
+# Copyright (C) 2021 Guido Flohr <guido.flohr@cantanea.com>,
+# all rights reserved.
+
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What the Fuck You Want
+# to Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
+
+# Make Dist::Zilla happy.
+# ABSTRACT: Representation of a chess position with move generator, legality checker etc.
+
+package Chess::Position;
+
+use strict;
+
+sub new {
+	my ($class) = @_;
+
+	my $self = {
+	};
+	bless $self, $class;
+}
+
+1;
