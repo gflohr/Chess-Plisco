289c320f1ef15fb8ec76559d1c827964b4fba171
Guido Flohr <guido.flohr@cantanea.com>
2021-08-15 19:37:16 +0300

    check properties

--- lib/Chess/Position/Macro.pm
+++ lib/Chess/Position/Macro.pm
@@ -43,6 +43,10 @@ define cp_pos_b_qcastle => '$p', '$p->[CP_POS_B_QCASTLE]';
 define cp_pos_ep_shift => '$p', '$p->[CP_POS_EP_SHIFT]';
 define cp_pos_half_move_clock => '$p', '$p->[CP_POS_HALF_MOVE_CLOCK]';
 define cp_pos_half_moves => '$p', '$p->[CP_POS_HALF_MOVES]';
+define cp_pos_w_king_shift => '$p', '$p->[CP_POS_W_KING_SHIFT]';
+define cp_pos_b_king_shift => '$p', '$p->[CP_POS_W_KING_SHIFT]';
+define cp_pos_in_check => '$p', '$p->[CP_POS_IN_CHECK]';
+define cp_pos_checkers => '$p', '$p->[CP_POS_CHECKERS]';
 
 define cp_move_to => '$m', '(($m) & 0x3f)';
 define cp_move_set_to => '$m', '$v', '(($m) = (($m) & ~0x3f) | (($v) & 0x3f))';
