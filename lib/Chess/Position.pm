8a7d16772db9c8542c0b6941a78021158d279696
Guido Flohr <guido.flohr@cantanea.com>
2021-08-16 19:46:47 +0300

    always set moving piece

--- lib/Chess/Position.pm
+++ lib/Chess/Position.pm
@@ -688,12 +688,12 @@ sub pseudoLegalMoves {
 	if ($king_mask & $king_from_mask) {
 		if ($self->[CP_POS_W_KCASTLE + $to_move]
 		    && ($king_side_crossing_mask & $empty)) {
-			push @moves, ($king_from << 6) | $king_side_dest_shift;
+			push @moves, ($king_from << 6 | CP_KING << 16) | $king_side_dest_shift;
 		}
 		if ($self->[CP_POS_W_QCASTLE + $to_move]
 		    && (!(($queen_side_crossing_mask | $queen_side_rook_crossing_mask)
 		         & $occupancy))) {
-			push @moves, ($king_from << 6) | $queen_side_dest_shift;
+			push @moves, ($king_from << 6 | CP_KING << 16) | $queen_side_dest_shift;
 		}
 	}
 
@@ -702,7 +702,7 @@ sub pseudoLegalMoves {
 	while ($knight_mask) {
 		my $from = cp_bb_count_trailing_zbits cp_bb_clear_but_least_set $knight_mask;
 
-		$base_move = $from << 6;
+		$base_move = ($from << 6 | CP_PAWN << 16);
 	
 		$target_mask = ~$my_pieces & $knight_attack_masks[$from];
 
@@ -716,7 +716,7 @@ sub pseudoLegalMoves {
 	while ($bishop_mask) {
 		my $from = cp_bb_count_trailing_zbits cp_bb_clear_but_least_set $bishop_mask;
 
-		$base_move = $from << 6;
+		$base_move = ($from << 6 | CP_BISHOP << 16);
 	
 		$target_mask = cp_mm_bmagic($from, $occupancy) & ($empty | $her_pieces);
 
@@ -730,7 +730,7 @@ sub pseudoLegalMoves {
 	while ($rook_mask) {
 		my $from = cp_bb_count_trailing_zbits cp_bb_clear_but_least_set $rook_mask;
 
-		$base_move = $from << 6;
+		$base_move = ($from << 6 | CP_ROOK << 16);
 	
 		$target_mask = cp_mm_rmagic($from, $occupancy) & ($empty | $her_pieces);
 
@@ -745,7 +745,7 @@ sub pseudoLegalMoves {
 	my $from = cp_bb_count_trailing_zbits $king_mask;
 
 	# FIXME! 6 should be a constant!
-	$base_move = $from << 6;
+	$base_move = ($from << 6 | CP_KING << 16);
 
 	$target_mask = ~$my_pieces & $king_attack_masks[$from];
 
@@ -770,7 +770,7 @@ sub pseudoLegalMoves {
 	while ($pawn_mask) {
 		my $from = cp_bb_count_trailing_zbits cp_bb_clear_but_least_set $pawn_mask;
 
-		$base_move = $from << 6;
+		$base_move = ($from << 6 | CP_PAWN << 16);
 		$target_mask = ($pawn_single_masks->[$from] & $empty)
 			| ($pawn_capture_masks->[$from] & ($her_pieces | $ep_target_mask));
 		_cp_moves_from_mask $target_mask, @moves, $base_move;
@@ -787,7 +787,7 @@ sub pseudoLegalMoves {
 			$target_mask = $pawn_double_masks->[$from] & $empty;
 			if ($target_mask) {
 				my $to = $from + ($offset << 1);
-				push @moves, $from << 6 | $to;
+				push @moves, ($from << 6) | $to | CP_PAWN << 16;
 			}
 		}
 		$pawn_mask = cp_bb_clear_least_set $pawn_mask;
@@ -820,29 +820,6 @@ sub doMove {
 }
 
 # Class methods.
-sub parseCoordinateNotation {
-	# FIXME! This should be a constructor of Chess::Position::Move
-	my ($self, $move) = @_;
-
-	return if $move !~ /^([a-h][1-8])([a-h][1-8])([qrbn])?$/;
-
-	my ($from, $to, $promote) = ($1, $2, $3);
-
-	my $move = $self->squareToShift($from) << 6 | $self->squareToShift($to);
-
-	if ($promote) {
-		my %pieces = (
-			q => CP_QUEEN,
-			r => CP_ROOK,
-			b => CP_BISHOP,
-			n => CP_KNIGHT,
-		);
-		$move |= ($pieces{$promote} << 13);
-	}
-
-	return $move;
-}
-
 sub dumpBitboard {
 	my (undef, $bitboard) = @_;
 
